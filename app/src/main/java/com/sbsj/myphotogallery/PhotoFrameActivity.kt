065898f1cefdf05f93831acf5e1cf4cb4798de33
master <simsungbo96@gmail.com>
2022-04-01 13:25:26 +0900

    에러수정

--- app/src/main/java/com/sbsj/myphotogallery/PhotoFrameActivity.kt
+++ app/src/main/java/com/sbsj/myphotogallery/PhotoFrameActivity.kt
@@ -4,13 +4,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import java.util.*
 import kotlin.concurrent.timer
 
 class PhotoFrameActivity : AppCompatActivity() {
 
     private val photoList = mutableListOf<Uri>()
     private var currentPosition = 0;
-
+    private var timer : Timer? =null
     private val photoImageView: ImageView by lazy {
         findViewById<ImageView>(R.id.photoImageView)
     }
@@ -25,7 +26,20 @@ class PhotoFrameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_frrame)
         getPhotoUriFromIntent()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer?.cancel()
+    }
+    override fun onStop() {
+        super.onStop()
+        timer?.cancel()
 
     }
 
@@ -51,9 +65,8 @@ class PhotoFrameActivity : AppCompatActivity() {
                     .alpha(1.0f)
                     .setDuration(1000)
                     .start()
-
                 currentPosition =next
-                
+
             }
         }
     }
