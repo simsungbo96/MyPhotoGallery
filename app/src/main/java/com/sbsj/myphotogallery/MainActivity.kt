7cd0903a24a6e13754be5507ffba06fb41fbbfe5
master <simsungbo96@gmail.com>
2022-04-01 11:04:21 +0900

    전자액자

---
+++ app/src/main/java/com/sbsj/myphotogallery/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.sbsj.myphotogallery
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy {
+        findViewById<Button>(R.id.addButton)
+    }
+
+    private val startPhotoFrameModeButton: Button by lazy {
+        findViewById<Button>(R.id.startPhotoFrameModeButton)
+    }
+    private val imageView: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView1))
+            add(findViewById(R.id.imageView2))
+            add(findViewById(R.id.imageView3))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+
+    }
+    private val imageUriList: MutableList<Uri> = mutableListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+
+
+    }
+    private fun initStartPhotoFrameModeButton() {
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this,PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed { index, uri ->
+                intent.putExtra("photo$index",uri.toString())
+            }
+            intent.putExtra("photoSize",imageUriList.size)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //권한이 잘부여됬을떄 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    //todo 교육용 팝업 확인 후 권한 팝업
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
+                }
+            }
+        }
+
+    }
+
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        when (requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+                    if(imageUriList.size >= 6){
+                        Toast.makeText(this,"사진이 꽉찼습니다.",Toast.LENGTH_SHORT).show()
+                    }
+                    imageUriList.add(selectedImageUri)
+                    imageView[imageUriList.size-1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    navigatePhotos()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자에 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+
+    }
+
+
+
+
+
+}
